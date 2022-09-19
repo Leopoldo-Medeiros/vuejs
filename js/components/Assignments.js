5b289ba7de2fead63ddcb158c03ca9da51e96478
Leo Medeiros <lfm@onlineapplication.io>
2022-09-19 17:44:14 +0100

    Parent-Child State Communication

--- js/components/Assignments.js
+++ js/components/Assignments.js
@@ -1,20 +1,15 @@
 import AssignmentList from "./AssignmentList.js";
+import AssignmentCreate from "./AssignmentCreate.js";
 
 export default {
-  components: { AssignmentList },
+  components: { AssignmentList, AssignmentCreate },
 
   template: `
         <section class="space-y-6">
             <assignment-list :assignments="filters.inProgress" title="In Progress"></assignment-list>
             <assignment-list :assignments="filters.completed" title="Completed"></assignment-list>
 
-          <!-- Calling the add method when the form is submitted. -->
-          <form @submit.prevent="add">
-            <div class="border border-gray-600 text-black">
-              <input v-model="newAssignment" placeholder="New Assignment ..." class="p-2" />
-              <button type="submit" class="bg-white p-2 border-l">Add</button>
-            </div>
-          </form>
+            <assignment-create @add="add"></assignment-create>
         </section>
     `,
 
@@ -25,31 +20,25 @@ export default {
         { name: "Read Chapter 4", complete: false, id: 2 },
         { name: "Turn in Homework", complete: false, id: 3 },
       ],
-
-      newAssignment: ''
     };
   },
 
   computed: {
-
     filters() {
       return {
-        inProgress: this.assignments.filter(assignment => ! assignment.complete),
-        completed: this.assignments.filter(assignment => assignment.complete)
+        inProgress: this.assignments.filter(assignment => !assignment.complete),
+        completed: this.assignments.filter(assignment => assignment.complete),
       };
-    }
+    },
   },
 
-
   methods: {
-    add() {
+    add(name) {
       this.assignments.push({
-        name: this.newAssignment,
+        name: name,
         completed: false,
-        id: this.assignments.length + 1
+        id: this.assignments.length + 1,
       });
-
-      this.newAssignment = '';
-    }
-  }
+    },
+  },
 };
