7767bb658117fa3b758afa8a492eddeefe058c34
Leo Medeiros <lfm@onlineapplication.io>
2022-09-21 14:02:33 +0100

    Filter Properties

--- js/components/AssignmentList.js
+++ js/components/AssignmentList.js
@@ -9,12 +9,27 @@ export default {
                 
       <!-- This is the section of the page that is displaying the assignments that are complete. -->
         <section v-show="assignments.length" class="mt-8">
-            <h2 class="font-bold mb-2">{{ title }}</h2>
+            <h2 class="font-bold mb-2">
+                {{ title }}
+                <span>({{ assignments.length }})</span>
+            </h2>
+
+            <!--  Creating a div with a class of flex gap-2. -->
+            <div class="flex gap-2">
+                <button 
+                @click="currentTag = tag"
+                v-for="tag in tags" 
+                class="border rounded px-1 py-px text-xs"
+                :class="{
+                    'border-blue-500 text-blue-500': tag === currentTag
+                }"
+                >{{ tag }}</button>
+            </div>
 
             <!-- Creating a border around the list of assignments. --> 
-            <ul class="border border-gray-600 divide-y divide-gray-600">
+            <ul class="border border-gray-600 divide-y divide-gray-600 mt-6">
                 <assignment 
-                    v-for="assignment in assignments"
+                    v-for="assignment in filteredAssignments"
                     :key="assignment.id"
                     :assignment="assignment"
                 ></assignment>    
@@ -27,4 +42,25 @@ export default {
     assignments: Array,
     title: String,
   },
+
+  data() {
+    return {
+      currentTag: 'all'
+    };
+  },
+
+  computed: {
+    filteredAssignments() {
+      if (this.currentTag === 'all') {
+        return this.assignments;
+      }
+
+      return this.assignments.filter(a => a.tag === this.currentTag);
+    },
+
+    tags() {
+       return ['all', ...new Set(this.assignments.map(a => a.tag))]; 
+    }
+  }
 };
+ 
