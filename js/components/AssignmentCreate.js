9d3910d8072b2eeaab65e356b9d93d4351fd20f0
leopo <leopoldof.medeiros@gmail.com>
2022-09-27 16:55:38 +0100

    Components with Slots and Flags

--- js/components/AssignmentCreate.js
+++ js/components/AssignmentCreate.js
@@ -2,7 +2,7 @@ export default {
   template: `
         <!-- Calling the add method when the form is submitted. -->
         <form @submit.prevent="add">
-          <div class="border border-gray-600 text-black">
+          <div class="border border-gray-600 text-black flex">
             <input v-model="newAssignment" placeholder="New Assignment ..." class="p-2" />
             <button type="submit" class="bg-white p-2 border-l">Add</button>
           </div>
--- js/components/AssignmentList.js
+++ js/components/AssignmentList.js
@@ -2,59 +2,67 @@ import Assignment from "./Assignment.js";
 import AssignmentTags from "./AssignmentTags.js";
 
 export default {
-  components: { Assignment, AssignmentTags },
+    components: {Assignment, AssignmentTags},
 
-  template: `
-        <!-- This is a section of the page that is displaying the assignments that are not complete. -->
-        <!-- Collection of assignment -->
-                
+    template: `
+      <!-- This is a section of the page that is displaying the assignments that are not complete. -->
+      <!-- Collection of assignment -->
+
       <!-- This is the section of the page that is displaying the assignments that are complete. -->
-        <section v-show="assignments.length" class="mt-8">
-            <h2 class="font-bold mb-2">
-                {{ title }}
-                <span>({{ assignments.length }})</span>
-            </h2>
-            
-            <!-- This part access all assignments -->
-            <!--  @change -> A way to pass data from a child component to a parent component. -->
+      <section v-show="assignments.length" class="w-60">
+      <div class="flex justify-between items-start">
+        <h2 class="font-bold mb-2">
+          {{ title }}
+          <span>({{ assignments.length }})</span>
+        </h2>
 
-            <!-- When this component anounces we change tags, we update our status too. -->
-            <assignment-tags 
-              v-model:currentTag="currentTag"
-              :initial-tags="assignments.map(a => a.tag)" 
-            />
+        <button v-show="canToggle" @click="$emit('toggle')">&times;</button>
+      </div>
 
-            <!-- Creating a border around the list of assignments. --> 
-            <ul class="border border-gray-600 divide-y divide-gray-600 mt-6">
-                <assignment 
-                    v-for="assignment in filteredAssignments"
-                    :key="assignment.id"
-                    :assignment="assignment"
-                ></assignment>    
-            </ul>
-        </section>
-        
+      <!-- This part access all assignments -->
+      <!--  @change -> A way to pass data from a child component to a parent component. -->
+
+      <!-- When this component announces we change tags, we update our status too. -->
+      <assignment-tags
+          v-model:currentTag="currentTag"
+          :initial-tags="assignments.map(a => a.tag)"
+      />
+
+      <!-- Creating a border around the list of assignments. -->
+      <ul class="border border-gray-600 divide-y divide-gray-600 mt-6">
+        <assignment
+            v-for="assignment in filteredAssignments"
+            :key="assignment.id"
+            :assignment="assignment"
+        ></assignment>
+      </ul>
+
+      <slot></slot>
+
+      </section>
+
     `,
 
-  props: {
-    assignments: Array,
-    title: String,
-  },
-
-  data() {
-    return {
-      /* tracking the current tag. */
-      currentTag: "all",
-    }
-  },
-
-  computed: {
-    filteredAssignments() {
-      if (this.currentTag === "all") {
-        return this.assignments;
-      }
-
-      return this.assignments.filter((a) => a.tag === this.currentTag);
+    props: {
+        assignments: Array,
+        title: String,
+        canToggle: { type: Boolean, default: false  }
     },
-  },
+
+    data() {
+        return {
+            /* tracking the current tag. */
+            currentTag: "all",
+        }
+    },
+
+    computed: {
+        filteredAssignments() {
+            if (this.currentTag === "all") {
+                return this.assignments;
+            }
+
+            return this.assignments.filter((a) => a.tag === this.currentTag);
+        },
+    },
 };
