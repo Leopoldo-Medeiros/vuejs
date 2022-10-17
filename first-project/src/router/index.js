abebfefdc0bccedb5943962bd48bd792c31a0246
Leopoldo Medeiros <32571375+Leopoldo-Medeiros@users.noreply.github.com>
2022-10-17 02:53:21 +0100

    Vite Section

---
+++ first-project/src/router/index.js
@@ -0,0 +1,33 @@
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import ContactView from "../views/ContactView.vue";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: HomeView,
+    },
+    {/* Creating a route to the AboutView.vue file. */
+      path: "/about",
+      name: "about",
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import("../views/AboutView.vue"),
+    },
+    {
+      /* This is creating a route to the ContactView.vue file. */
+      path: "/contact",
+      name: "contact",
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import("../views/ContactView.vue"),
+    },
+  ],
+});
+
+export default router;
